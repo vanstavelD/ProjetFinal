6983dd44255ff0bcea283ba6d7fbddc8d1fa0525
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2022-10-13 14:16:30 +0200

    first commit

---
+++ projetfinal/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nh+!xjqh9xv^$7+sw_h0=!l9@rvt2z+7wd-lqm8=lz!e1fv!bb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
